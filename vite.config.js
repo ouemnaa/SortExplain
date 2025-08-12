47a023d13c3ff85849e84d3ffc5964277ee51e15
ouemnaa <emna.ouertani@etudiant-fst.utm.tn>
2025-08-12 17:14:17 +0100

    second

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,23 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  // Temporarily remove base path to test asset loading
+  // base: '/SortExplain/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'static',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'static/[name].[hash].[ext]',
+        chunkFileNames: 'static/[name].[hash].js',
+        entryFileNames: 'static/[name].[hash].js',
+      },
     },
   },
 })
